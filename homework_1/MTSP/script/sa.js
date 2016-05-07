da3eace8f612984c0ea9eb4d9244320e59f7b618
Kim Mok <cafe.mui@gmail.com>
2016-05-06 23:18:02 -0400

    rename j()-> nextSalesmen(), and created initialDistance() to computer the cost of the initial route for each salesmen

--- homework_1/MTSP/script/sa.js
+++ homework_1/MTSP/script/sa.js
@@ -19,17 +19,15 @@ $(document).ready(function(){
 	//var sa = new SimulatedAnnealing(1000, 0.9999, city.city_list);
 	//var sa = new SimulatedAnnealing(1000, 0.99, city.city_list);
 	//var sa = new SimulatedAnnealing(50, 0.99, city.city_list);
-	var sa = new SimulatedAnnealing(100, 0.99, city.city_list);
+	var sa = new SimulatedAnnealing(50, 0.99, city.city_list);
 
 	document.getElementById("temperature").value = 1000;
 	document.getElementById("cool_rate").value = 0.9999;
 	document.getElementById("total_cities").value = 50;
-	
-	sa.k_means(city.city_list);
 
 	document.getElementById("start_path").onclick = function(){
 		console.log("start!");
-		sa.start();
+		sa.nextSalesmen();
 	}
 
 });
@@ -41,7 +39,159 @@ class City {
 		this.x = x_coord;
 		this.y = y_coord;
 		
-
+		/*
+		this.city_list = [{'x': this.x, 'y': this.y},
+						  {'x': 325, 'y': 298},
+						  {'x': 231, 'y': 110},
+						  {'x': 485, 'y': 226},
+						  {'x': 176, 'y': 424},
+						  {'x': 95,  'y': 98},
+						  {'x': 477, 'y': 361},
+						  {'x': 490, 'y': 120},
+						  {'x': 397, 'y': 414},
+						  {'x': 408, 'y': 73},
+						  {'x': 439, 'y': 218},
+						  {'x': 139, 'y': 33},
+						  {'x': 389, 'y': 314},
+						  {'x': 44,  'y': 204},
+						  {'x': 222, 'y': 207},
+						  {'x': 298, 'y': 358},
+						  {'x': 134, 'y': 481},
+						  {'x': 458, 'y': 416},
+						  {'x': 427, 'y': 264},
+						  {'x': 414, 'y': 201},
+						  {'x': 256, 'y': 202},
+						  {'x': 183, 'y': 462},
+						  {'x': 69,  'y': 463},
+						  {'x': 346, 'y': 446},
+						  {'x': 285, 'y': 81},
+						  {'x': 478, 'y': 36},
+						  {'x': 157, 'y': 361},
+						  {'x': 211, 'y': 142},
+						  {'x': 122, 'y': 184},
+						  {'x': 19,  'y': 275},
+						  {'x': 90,  'y': 205},
+						  {'x': 464, 'y': 12},
+						  {'x': 33,  'y': 269},
+						  {'x': 355, 'y': 442},
+						  {'x': 128, 'y': 315},
+						  {'x': 334, 'y': 57},
+						  {'x': 75,  'y': 226},
+						  {'x': 249, 'y': 473},
+						  {'x': 302, 'y': 143},
+						  {'x': 302, 'y': 149},
+						  {'x': 132, 'y': 88},
+						  {'x': 214, 'y': 336},
+						  {'x': 116, 'y': 421},
+						  {'x': 54,  'y': 71},
+						  {'x': 469, 'y': 129},
+						  {'x': 350, 'y': 179},
+						  {'x': 244, 'y': 101},
+						  {'x': 87,  'y': 420},
+						  {'x': 256, 'y': 471},
+						  {'x': 13,  'y': 320},
+						  {'x': 234, 'y': 343},
+						  {'x': 296, 'y': 75},
+						  {'x': 435, 'y': 162},
+						  {'x': 258, 'y': 486},
+						  {'x': 123, 'y': 13},
+						  {'x': 112, 'y': 479},
+						  {'x': 422, 'y': 199},
+						  {'x': 87,  'y': 195},
+						  {'x': 8,   'y': 438},
+						  {'x': 71,  'y': 11},
+						  {'x': 248, 'y': 235},
+						  {'x': 89,  'y': 316},
+						  {'x': 378, 'y': 468},
+						  {'x': 40,  'y': 164},
+						  {'x': 333, 'y': 377},
+						  {'x': 453, 'y': 455},
+						  {'x': 390, 'y': 15},
+						  {'x': 187, 'y': 485},
+						  {'x': 482, 'y': 46},
+						  {'x': 166, 'y': 14},
+						  {'x': 302, 'y': 297},
+						  {'x': 382, 'y': 160},
+						  {'x': 389, 'y': 46},
+						  {'x': 339, 'y': 353},
+						  {'x': 233, 'y': 76},
+						  {'x': 353, 'y': 188},
+						  {'x': 354, 'y': 197},
+						  {'x': 400, 'y': 285},
+						  {'x': 350, 'y': 301},
+						  {'x': 241, 'y': 243},
+						  {'x': 68,  'y': 266},
+						  {'x': 328, 'y': 273},
+						  {'x': 409, 'y': 36},
+						  {'x': 223, 'y': 347},
+						  {'x': 119, 'y': 213},
+						  {'x': 308, 'y': 80},
+						  {'x': 109, 'y': 187},
+						  {'x': 190, 'y': 393},
+						  {'x': 214, 'y': 264},
+						  {'x': 175, 'y': 242},
+						  {'x': 47,  'y': 279},
+						  {'x': 165, 'y': 148},
+						  {'x': 287, 'y': 347},
+						  {'x': 144, 'y': 444},
+						  {'x': 381, 'y': 21},
+						  {'x': 411, 'y': 480},
+						  {'x': 259, 'y': 84},
+						  {'x': 159, 'y': 110},
+						  {'x': 276, 'y': 484},
+						  {'x': 113, 'y': 142},
+						  {'x': 277, 'y': 85},
+						  {'x': 236, 'y': 423},
+						  {'x': 323, 'y': 198},
+						  {'x': 415, 'y': 273},
+						  {'x': 37,  'y': 98},
+						  {'x': 42,  'y': 4},
+						  {'x': 244, 'y': 55},
+						  {'x': 332, 'y': 468},
+						  {'x': 249, 'y': 205},
+						  {'x': 5,   'y': 171},
+						  {'x': 163, 'y': 143},
+						  {'x': 222, 'y': 67},
+						  {'x': 350, 'y': 146},
+						  {'x': 271, 'y': 432},
+						  {'x': 146, 'y': 4},
+						  {'x': 135, 'y': 356},
+						  {'x': 490, 'y': 52},
+						  {'x': 488, 'y': 2},
+						  {'x': 114, 'y': 124},
+						  {'x': 444, 'y': 307},
+						  {'x': 459, 'y': 296},
+						  {'x': 104, 'y': 93},
+						  {'x': 393, 'y': 275},
+						  {'x': 118, 'y': 428},
+						  {'x': 50,  'y': 200},
+						  {'x': 46,  'y': 1},
+						  {'x': 178, 'y': 390},
+						  {'x': 483, 'y': 56},
+						  {'x': 82,  'y': 123},
+						  {'x': 41,  'y': 116},
+						  {'x': 6,   'y': 347},
+						  {'x': 350, 'y': 357},
+						  {'x': 449, 'y': 276},
+						  {'x': 232, 'y': 354},
+						  {'x': 198, 'y': 322},
+						  {'x': 292, 'y': 318},
+						  {'x': 430, 'y': 343},
+						  {'x': 388, 'y': 169},
+						  {'x': 176, 'y': 95},
+						  {'x': 275, 'y': 111},
+						  {'x': 93,  'y': 55},
+						  {'x': 77,  'y': 33},
+						  {'x': 377, 'y': 33},
+						  {'x': 229, 'y': 194},
+						  {'x': 107, 'y': 140},
+						  {'x': 460, 'y': 273},
+						  {'x': 212, 'y': 21},
+						  {'x': 371, 'y': 57},
+						  {'x': 447, 'y': 235},
+						  {'x': 80,  'y': 2},
+						  ]; 
+		*/
 		
 		this.city_list = [{'x': this.x, 'y': this.y},
 						  {'x': 244, 'y': 101},
@@ -99,13 +249,6 @@ class SimulatedAnnealing {
 		this.original_temp = temp;
 		this.cooling = cooling;
 
-
-		this.best_solution = cities; //keeps track of current path/route
-
-		//this.best_cost = this.getCost(this.best_solution); //keeps track of the best cost
-		this.best_cost = [];
-
-		this.best = cities; //set current cities to best
 		this.cities = cities;
 
 		//this.initial_distance = 0;
@@ -113,27 +256,38 @@ class SimulatedAnnealing {
 
 		this.current = cities;
 
-		this.centroids = [];
-		this.clusters  = [];
+		var temp = this.k_means(cities);
+
+		this.centroids = temp["centroids"];
+
+		this.clusters  = temp["clusters"]; // grouped cities that for each salesman
+
+		this.best_solution = this.clusters; //keeps track of current path/route for each salesmen
+
+		this.initial_distance = this.initialDistance(this.centroids, this.clusters);
+
+		this.best_cost = jQuery.extend([], this.initial_distance);
 
 		this.track = 0;
 
+		console.log("initialDistance(): ", this.initial_distance);
+
 	}
 
 
-	start(){
-		this.best_solution = this.clusters;
+	initialDistance(salesmen, clusters){
+		//Returns the cost of the initial cities assign to each salesmen
 
-		for (var center=0; center <this.centroids.length; center++){
-			this.initial_distance[center] = this.getCost(this.clusters[center]);
+		var initial_distance=[]; 
+
+		for (var i=0; i <salesmen.length; i++){
+			initial_distance[i] = this.getCost(clusters[i]);
 		}
 
-		this.best_cost = jQuery.extend([], this.initial_distance);
-		
-		this.j();
+		return initial_distance;
 	}
 
-	j(){
+	nextSalesmen(){
 		console.log("loop: ", this.track, "<",this.centroids.length);
 		if (this.track < this.centroids.length){
 			console.log("c0: ",this.track);
@@ -146,30 +300,6 @@ class SimulatedAnnealing {
 			}, 100);
 			
 		}
-
-/*
-		var _this = this;
-
-		setTimeout(function(){
-			_this.track += 1;
-			_this.j();
-
-			if (_this.track < _this.centroids.length){
-				console.log("HELLO2");
-				
-			
-				if (toggle == 800000){
-					console.log("toggle");
-					toggle = 100;
-				}
-				else{
-					toggle = 800000;
-					console.log("toggle1");
-				}
-			
-			}
-		}, toggle);
-		*/
 	}
 
 	anneal(center){
@@ -221,7 +351,7 @@ class SimulatedAnnealing {
 			console.log("Initial cost: ", this.initial_distance);
 			console.log("Best cost: ", this.best_cost);
 			this.track += 1;
-			this.j();
+			this.nextSalesmen();
 
 		}
 
@@ -334,40 +464,6 @@ class SimulatedAnnealing {
 				context.stroke();
 			}
 		},300);
-
-/*		
-		for (let cluster of this.best_solution){
-			context.strokeStyle = "#ff99c2";
-			context.beginPath();
-
-			for (var i=0; i <cluster.length-1; i++){
-				
-				context.moveTo(cluster[i].x, cluster[i].y);
-				context.lineTo(cluster[i+1].x, cluster[i+1].y);
-			}
-			context.stroke();
-
-			context.beginPath();
-			context.strokeStyle = "green";
-			context.fillStyle = "green";
-
-			context.moveTo(cluster[cluster.length-1].x, cluster[cluster.length-1].y);
-			context.lineTo(cluster[0].x, cluster[0].y);
-			context.stroke();
-		}
-
-
-		context.strokeStyle = "#ff80ff";
-		context.fillStyle = "#ff80ff";
-
-		for (let p of this.centroids) {
-			context.beginPath();
-			context.arc(p.x, p.y, 2, 0, 2 * Math.PI);
-			context.fill();
-			context.stroke();
-		}
-*/
-
 	}
 
 
@@ -392,7 +488,14 @@ class SimulatedAnnealing {
 
 	
 	k_means(cities){
-		var n = jQuery.extend([], this.cities);
+		/*
+			Returns a `result` list containing the centroids and clusters.
+		*/
+
+		var result = [];
+
+		var n = jQuery.extend([], cities);
+		
 		var k = 2;
 
 		var centroids = [];
@@ -469,8 +572,8 @@ class SimulatedAnnealing {
 			context.stroke();
 		}
 
-		this.clusters = clusters;
-		this.centroids = centroids;
+		result["clusters"] = clusters;
+		result["centroids"] = centroids;
 
 
 		context.strokeStyle = "blue";
@@ -488,6 +591,8 @@ class SimulatedAnnealing {
 		}
 
 		context.stroke();
+
+		return result;
 
 	}
 
