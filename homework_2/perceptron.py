cb5963a9bb783d57fac307c9cc788f1a2f890c70
Kim Mok <cafe.mui@gmail.com>
2016-05-07 20:49:19 -0400

    removed unused code

--- homework_2/perceptron.py
+++ homework_2/perceptron.py
@@ -5,43 +5,61 @@ from numpy import dot, array, random
 # [o x]
 
 #activation function
-output = lambda y: -1 if y > theta  else 0 if -theta <= y <= theta else 1
+output = lambda y: -1 if y < 0 else 1 if y > 0 else 0
 
 #initialize weights randomly
-weight = random.random(3)
+weight = random.random(5)
 
-#threshold theta
-theta = weight[len(weight)-1]
-
-#set learning rate a to 1 for simplicity
-a = 1.0
+#set learning rate alpha to 1 for simplicity
+alpha = 1
 
 #number of iterations it take for no weight changes
 iterations = 0
 
 #[[bipolar inputs, bias], bipolar target]
 training_pattern = [
-					[[-1, -1, -1, -1, 1], -1],
-					[[-1, -1, -1,  1, 1], -1],
-					[[-1, -1,  1, -1, 1], -1],
-					[[-1, -1,  1,  1, 1], -1],
-					[[-1,  1, -1, -1, 1], -1],
-					[[-1,  1, -1,  1, 1],  1],
-					[[-1,  1,  1, -1, 1], -1],
-					[[-1,  1,  1,  1, 1], -1],
-					[[ 1, -1, -1, -1, 1], -1],
-					[[ 1, -1, -1,  1, 1], -1],
-					[[ 1, -1,  1, -1, 1], -1],
-					[[ 1, -1,  1,  1, 1], -1],
-					[[ 1,  1, -1, -1, 1], -1],
-					[[ 1,  1, -1,  1, 1], -1],
-					[[ 1,  1,  1, -1, 1], -1],
-					[[ 1,  1,  1,  1, 1], -1]
+					[array([-1, -1, -1, -1, 1]), -1],
+					[array([-1, -1, -1,  1, 1]), -1],
+					[array([-1, -1,  1, -1, 1]), -1],
+					[array([-1, -1,  1,  1, 1]), -1],
+					[array([-1,  1, -1, -1, 1]), -1],
+					[array([-1,  1, -1,  1, 1]),  1],
+					[array([-1,  1,  1, -1, 1]), -1],
+					[array([-1,  1,  1,  1, 1]), -1],
+					[array([ 1, -1, -1, -1, 1]), -1],
+					[array([ 1, -1, -1,  1, 1]), -1],
+					[array([ 1, -1,  1, -1, 1]), -1],
+					[array([ 1, -1,  1,  1, 1]), -1],
+					[array([ 1,  1, -1, -1, 1]), -1],
+					[array([ 1,  1, -1,  1, 1]), -1],
+					[array([ 1,  1,  1, -1, 1]), -1],
+					[array([ 1,  1,  1,  1, 1]), -1]
 				   ]
 
 
+print "initial weights: ", weight
+
+while (1):
+	#count total errors
+	error_count = 0
+
+	iterations += 1
+
+	for i, target in training_pattern:
+		xw = dot(i, weight)
+		y_in = output(xw)
 
 
+		if target - y_in != 0:
+			#update weights
+			weight = weight + alpha*(target-y_in) * i
+			error_count += 1
+			print "change weights"
+		
+	if error_count == 0:
+		break
 
-print "theta: ", theta
-print "output: ", output(1)
+
+#print "theta: ", theta
+print "iterations: ", iterations
+print "final weights: ", weight
