8e873eb4977126725fabd08ad6a4f76b4ca01383
Kim Mok <cafe.mui@gmail.com>
2016-05-18 19:15:54 -0400

    made corrections to extractData() and initiateRandomWeights()

--- homework_2/test2.py
+++ homework_2/test2.py
@@ -2,6 +2,7 @@ from numpy import dot, array, random, concatenate, asarray
 import itertools
 
 def extractData():
+	#Returns a list of input letters and its corresponding output and the character
 	input_raw_bipolar_vals = []
 	temp = []
 
@@ -10,8 +11,24 @@ def extractData():
 	for char in file.read():
 		if char == "\n":
 			pass
-		if char == "a" or char == "b" or char == "c" or char == "e" or char == "j" or char == "k":
-			input_raw_bipolar_vals.append((array(temp), 1, char))
+		#if char == "a" or char == "b" or char == "c" or char == "e" or char == "j" or char == "k":
+		if char == "a":
+			input_raw_bipolar_vals.append((array(temp), array([1, -1, -1, -1 ,-1 ,-1]), char))
+			temp = []
+		if char == "b":
+			input_raw_bipolar_vals.append((array(temp), array([-1, 1, -1, -1 ,-1 ,-1]), char))
+			temp = []
+		if char == "c":
+			input_raw_bipolar_vals.append((array(temp), array([-1, -1, 1, -1 ,-1 ,-1]), char))
+			temp = []
+		if char == "e":
+			input_raw_bipolar_vals.append((array(temp), array([-1, -1, -1, 1 ,-1 ,-1]), char))
+			temp = []
+		if char == "j":
+			input_raw_bipolar_vals.append((array(temp), array([-1, -1, -1, -1 ,1 ,-1]), char))
+			temp = []
+		if char == "k":
+			input_raw_bipolar_vals.append((array(temp), array([-1, -1, -1, -1 ,-1 ,1]), char))
 			temp = []
 		if char == "_":
 			temp.append(-1)
@@ -20,8 +37,16 @@ def extractData():
 
 	return input_raw_bipolar_vals
 
-def initiateRandomWeights(n):
-	return random.random(n)
+def initiateRandomWeights(num_letters):
+	#Returns a matrix containing random weights for corresponding letters
+	weights = []
+	for j in range(num_letters):
+		temp = []
+		for i in range(63):
+			temp.append(random.random())
+		weights.append(temp)
+
+	return weights
 
 def trainingSetInputs(inputs):
 	training_data = []
@@ -39,7 +64,8 @@ def trainingSetInputs(inputs):
 	return training_data
 
 
+#get the training data
 training = extractData()
-training_set = trainingSetInputs(training)
-weight = initiateRandomWeights(64)
-print weight
+
+#get the weights for the data
+weights = initiateRandomWeights(6)
